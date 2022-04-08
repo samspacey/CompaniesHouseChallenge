27baf0a3361d55328fc034666c72723ae1d24bea
-loper-sspacey <samspacey@icloud.com>
2022-04-08 13:36:36 +0100

    Finished loop and added extra files

--- APIRequests.py
+++ APIRequests.py
@@ -1,7 +1,17 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+
 def APIRequest(CompanyNumber):
+    BaseURL = "https://api.company-information.service.gov.uk"
 
     response = requests.get(
-    BaseURL + f'/company/{CompanyNumber}/persons-with-significant-control/',
-    auth=HTTPBasicAuth('db9f7d86-ef5f-4157-b79a-25104fa9c4fe', '')
+        BaseURL + f'/company/{CompanyNumber}/persons-with-significant-control/',
+        auth=HTTPBasicAuth('db9f7d86-ef5f-4157-b79a-25104fa9c4fe', '')
     )
-    return response
+
+    if 'errors' in response.json():
+        return "00000000"
+
+    for item in response.json()["items"]:
+        return item["identification"]["registration_number"]
--- Code.py
+++ Code.py
@@ -1,23 +1,14 @@
 import requests
 import json
+from APIRequests import APIRequest
+
 
 from requests.auth import HTTPBasicAuth
 
 
-BaseURL = "https://api.company-information.service.gov.uk"
+companynumber = "02373469"
 
-response = requests.get(
-    BaseURL + '/company/02373469/persons-with-significant-control/',
-    auth=HTTPBasicAuth('db9f7d86-ef5f-4157-b79a-25104fa9c4fe', '')
-)
-
-jsonstring = json.dumps(response.json())
-parsed = json.loads(jsonstring)
-print(json.dumps(parsed, indent=2, sort_keys=True))
-
-for item in response.json()["items"]:
-
-    print(item["name"])
-
-while companyname != "empty"
-
+while companynumber != "00000000":
+    #APIRequest(companynumber.rjust(8, "0"))
+    companynumber = APIRequest(companynumber.rjust(8, "0"))
+    print(companynumber)
